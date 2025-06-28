2af7b35e7dd50f7a47d9aa4899c40b0371bf6c5b
gabrielsilper <gabrielsilper.main@gmail.com>
2025-06-28 18:27:27 -0400

    chore: adjust name property of ErrorMessageResponse

--- src/main/java/com/github/gabrielsilper/BlogPost/models/dtos/ErrorMessageResponse.java
+++ src/main/java/com/github/gabrielsilper/BlogPost/models/dtos/ErrorMessageResponse.java
@@ -1,4 +1,4 @@
 package com.github.gabrielsilper.BlogPost.models.dtos;
 
-public record ErrorMessageResponse(String message, int status) {
+public record ErrorMessageResponse(String error, int status) {
 }
